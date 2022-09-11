dff36278dc666757a1f7ae074a671714019e4e7a
Gonzo <gonzalovidal.dev@gmail.com>
2022-09-10 23:10:43 -0500

    Add setup for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/calculator-tdd/',
   plugins: [react()], // vite config to work with react
   test: {
     // vitest config
